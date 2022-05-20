04bb5090fe55fa1f3305fe87663505445f7c6b5e
abhishek8173 <abhishekpandey19112000@gmail.com>
2022-05-20 19:23:43 +0530

    Updates

---
+++ precache-manifest.8708cc711a615b9c7d221c26140f0f5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19b2be18f0d9a1b21e0c",
+    "url": "/static/css/main.2ef193e5.chunk.css"
+  },
+  {
+    "revision": "19b2be18f0d9a1b21e0c",
+    "url": "/static/js/main.19b2be18.chunk.js"
+  },
+  {
+    "revision": "b570389d8cc2d5925792",
+    "url": "/static/js/1.b570389d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fa548954bb9cbd5e23eeb1356301b572",
+    "url": "/index.html"
+  }
+];
